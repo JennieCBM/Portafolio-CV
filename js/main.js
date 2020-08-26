5129742aa4aee89e95e71cc478306a577ff88158
Jennie <jenniebuitrago@gmail.com>
2020-08-26 20:55:35 -0300

    mod..

--- js/main.js
+++ js/main.js
@@ -22,8 +22,23 @@ const logo = document.querySelector('.brand-logo');
 const h1= document.querySelector('h1');
 const p = h1.parentNode.children[1];
 const lenguajes = document.querySelector('.lenguajes');
+console.log(scrollY);
+  if(window.scrollY > 3){
+    navbar.classList.remove("transparent");
+    navbar.style.background = "rgba(245, 220, 220, .6)";
+
+  }else{
+    navbar.classList.add("transparent");
+  };
+  if(window.scrollY > 10){
+    p.style.opacity = '0';
+    h1.style.opacity = '0';
+  }else{
+    p.style.opacity = '1';
+    h1.style.opacity = '1';
+  }
+
 if(scrollY > 680 && scrollY < 1073){
-  console.log(lenguajes.children)
   lenguajes.children[1].style.opacity='1';
   lenguajes.children[2].style.opacity='1';
 }else{
@@ -72,22 +87,7 @@ if(scrollY > 2180 && scrollY < 2495){
   lenguajes.children[15].style.opacity='.5';
   lenguajes.children[16].style.opacity='.5';
 };
-console.log(scrollY);
-  if(window.scrollY > 3){
-    navbar.classList.remove("transparent");
-    navbar.style.background = "rgba(245, 220, 220, .6)";
-    h1.style.opacity = '1';
-  };
-  if(window.scrollY < 3){
-    navbar.classList.add("transparent");
-    h1.style.opacity = "0";
-  };
-  if(window.scrollY > 6){
-    p.style.opacity = '1';
-  };
-  if(window.scrollY < 6){
-    p.style.opacity = "0";
-  };
+
 
 })
 //------------DATOS PERSONALES-----------------
