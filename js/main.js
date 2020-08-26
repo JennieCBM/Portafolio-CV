40d65f51a935c9727ee99100aa921169f7b367ca
Jennie <jenniebuitrago@gmail.com>
2020-08-26 20:14:46 -0300

    animaciones

--- js/main.js
+++ js/main.js
@@ -21,6 +21,58 @@ const navbar = document.querySelector('.nav');
 const logo = document.querySelector('.brand-logo');
 const h1= document.querySelector('h1');
 const p = h1.parentNode.children[1];
+const lenguajes = document.querySelector('.lenguajes');
+if(scrollY > 680 && scrollY < 1073){
+  console.log(lenguajes.children)
+  lenguajes.children[1].style.opacity='1';
+  lenguajes.children[2].style.opacity='1';
+}else{
+  lenguajes.children[1].style.opacity='.5';
+  lenguajes.children[2].style.opacity='.5';
+};
+if(scrollY > 1000 && scrollY < 1285){
+  lenguajes.children[3].style.opacity='1';
+  lenguajes.children[4].style.opacity='1';
+}else{
+  lenguajes.children[3].style.opacity='.5';
+  lenguajes.children[4].style.opacity='.5';
+};
+if(scrollY > 1180 && scrollY < 1498){
+  lenguajes.children[5].style.opacity='1';
+  lenguajes.children[6].style.opacity='1';
+}else{
+  lenguajes.children[5].style.opacity='.5';
+  lenguajes.children[6].style.opacity='.5';
+};
+if(scrollY > 1500 && scrollY < 1800){
+  lenguajes.children[8].style.opacity='1';
+  lenguajes.children[9].style.opacity='1';
+}else{
+  lenguajes.children[8].style.opacity='.5';
+  lenguajes.children[9].style.opacity='.5';
+};
+if(scrollY > 1680 && scrollY < 2010){
+  lenguajes.children[10].style.opacity='1';
+  lenguajes.children[11].style.opacity='1';
+}else{
+  lenguajes.children[10].style.opacity='.5';
+  lenguajes.children[11].style.opacity='.5';
+};
+if(scrollY > 1880 && scrollY < 2210){
+  lenguajes.children[12].style.opacity='1';
+  lenguajes.children[13].style.opacity='1';
+}else{
+  lenguajes.children[12].style.opacity='.5';
+  lenguajes.children[13].style.opacity='.5';
+};
+if(scrollY > 2180 && scrollY < 2495){
+  lenguajes.children[15].style.opacity='1';
+  lenguajes.children[16].style.opacity='1';
+}else{
+  lenguajes.children[15].style.opacity='.5';
+  lenguajes.children[16].style.opacity='.5';
+};
+console.log(scrollY);
   if(window.scrollY > 3){
     navbar.classList.remove("transparent");
     navbar.style.background = "rgba(245, 220, 220, .6)";
