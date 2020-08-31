c1e465ea638957b797f2440eb66869e6dd66a974
Jennie <jenniebuitrago@gmail.com>
2020-08-31 18:24:43 -0300

    modificaciones

--- js/main.js
+++ js/main.js
@@ -1,93 +1,112 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-   const elementos = document.querySelectorAll('.carousel');
-   M.Carousel.init(elementos, {
-     duration: 500,
-     indicators: true,
-     fullWidth: false,
-     shift: 50,
-     dist: 0
-   });
-   const elems = document.querySelectorAll('.sidenav');
-   const instances = M.Sidenav.init(elems);
+//-----------SPA----------------------------
+const links = document.querySelectorAll('.enlace');//los enlaces de la navbar
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
+const main = document.querySelector('#main');//la seccion a la que se apendeara cada contenido
+
+const h1= document.querySelector('h1');//el titulo
+
+const parrafo = h1.parentNode.children[1];//el subtitulo
+
+//declaro mi funcion ajax con dos argumentos, la url y el metodo (get por default)
+let ajax = function(url, metodo){
+  let http_metodo = metodo || "GET";
+  let xhr = new XMLHttpRequest;
+  xhr.open(http_metodo, url);
+  xhr.send();
+  return xhr;
+};
+//Al hacer click en cada enlace le asigno un valor al hash, será igual al id del target (enlace seleccionado)
+links.forEach((link) => {
+  link.addEventListener("click", (e)=>{
+    e.preventDefault();
+    window.location.hash = e.target.id;
+    //***********2.2 remuevo y agrego la clase que anima el titulo y subtitulo (otra opcion valida) comente esto para evitar dividir una pequeña funcion de agregar o remover clases en dos fragmentos separados
+    //h1.classList.remove("aparece");
+    //parrafo.classList.remove('aparece');
   });
 });
+//declaro una funcion para hacer cada peticion, y apendear el response al main
+let navegar = function(){
+  //concateno todo para usarlo en la petición, sin necesidad de que la ruta exacta sea visible en el pathname
+  let p = location.hash.split('#')[1]+ ".html";
+  let xhr = ajax(p);
+  xhr.addEventListener("load", ()=>{
+      if(xhr.status == 200){
+        main.innerHTML = xhr.response;
+        //***********1.2 remuevo y agrego la clase que anima el titulo y subtitulo, para evitar dividir una pequeña funcion de agregar y remover clases en dos fragmentos utilizo la propiedad offsetWidth la cual hace una lectura del ancho de un elemento en particular, al realizar dicha accion se trigerea en reflujo del DOM (se recarga un elemento especifico) y eso permite que la animacion vuelva a ejecutarse, agrego el operador void para minimizar el peso indicandole al navegador que el retorno sea undefined
+        h1.classList.remove("aparece");
+        parrafo.classList.remove('aparece');
+        void (h1.offsetWidth);
+        void (parrafo.offsetWidth);
+        h1.classList.add("aparece");
+        parrafo.classList.add('aparece');
+        if(p == "proyectos.html"){
+          //si estoy en la seccion de proyectos llamo a la funcion que da movimiento al carousel (MATERIALIZE)
+          const elementos = document.querySelectorAll('.carousel');
+           M.Carousel.init(elementos, {
+             dist: -50,
+             padding: 20,
+           });//cierre funcion carousel
+          //Cambio titulo y subtitulo
+          h1.innerText = "PROYECTOS";
+          parrafo.innerText = '"En mi proceso de formación me he encontrado con la oportunidad de poner en practica la superacion de pequeños retos, algunos de los siguientes proyectos son resultado de ese proceso..."'
+        }else{
+          h1.innerText = "Hola! bienvenido a mi sitio.";
+          parrafo.innerText = '"Soy desarrolladora front-end en formación con conocimientos intermedios de maquetacion, orientada al desarrollo de sitios webs responsive, que permitan al usuario una experiencia fluida y dinamica..."'
+        };//cierre condicional proyectos
+       };//cierre condicional status 200
+      //hago un bucle para cambiar el color al enlace seleccionado
+      links.forEach((link) => {
+        if(location.hash !== ""){
+          if(link.id == location.hash.split('#')[1]){
+            link.classList.add('activo');
+          }else{
+            link.classList.remove('activo');
+          }//cierre condicional para agregar clase activo
+        }//cierre condicional hash
+      });//cierre foreach
+  });//cierre de load
+};//cierre funcion navegar
 
-window.addEventListener("scroll", ()=>{
-const navbar = document.querySelector('.nav');
-const logo = document.querySelector('.brand-logo');
-const h1= document.querySelector('h1');
-const p = h1.parentNode.children[1];
-const lenguajes = document.querySelector('.lenguajes');
-console.log(scrollY);
-  if(window.scrollY > 3){
-    navbar.classList.remove("transparent");
-    navbar.style.background = "rgba(245, 220, 220, .6)";
-
-  }else{
-    navbar.classList.add("transparent");
-  };
-  if(window.scrollY > 10){
-    p.style.opacity = '0';
-    h1.style.opacity = '0';
-  }else{
-    p.style.opacity = '1';
-    h1.style.opacity = '1';
+//declaro una variable que es igual a la ejecucion de mi funcion ajax con la pagina inicial, para que se apendee el contenido inicial cada vez que se inicia solo con el dominio (sin ruta especifica)
+let pagina_inicial = ajax("home.html");
+pagina_inicial.addEventListener("load", ()=>{
+  if(pagina_inicial.status == 200){
+    main.innerHTML= pagina_inicial.response;
   }
-
-if(scrollY > 680 && scrollY < 1073){
-  lenguajes.children[1].style.opacity='1';
-  lenguajes.children[2].style.opacity='1';
-}else{
-  lenguajes.children[1].style.opacity='.5';
-  lenguajes.children[2].style.opacity='.5';
+});
+//siempre que location.hash tenga un valor, ejecuto navegar, esto permite al usuario acceder con la ruta directamente
+if(location.hash){
+  navegar();
 };
-if(scrollY > 1000 && scrollY < 1285){
-  lenguajes.children[3].style.opacity='1';
-  lenguajes.children[4].style.opacity='1';
-}else{
-  lenguajes.children[3].style.opacity='.5';
-  lenguajes.children[4].style.opacity='.5';
-};
-if(scrollY > 1180 && scrollY < 1498){
-  lenguajes.children[5].style.opacity='1';
-  lenguajes.children[6].style.opacity='1';
-}else{
-  lenguajes.children[5].style.opacity='.5';
-  lenguajes.children[6].style.opacity='.5';
-};
-if(scrollY > 1500 && scrollY < 1800){
-  lenguajes.children[8].style.opacity='1';
-  lenguajes.children[9].style.opacity='1';
-}else{
-  lenguajes.children[8].style.opacity='.5';
-  lenguajes.children[9].style.opacity='.5';
-};
-if(scrollY > 1680 && scrollY < 2010){
-  lenguajes.children[10].style.opacity='1';
-  lenguajes.children[11].style.opacity='1';
-}else{
-  lenguajes.children[10].style.opacity='.5';
-  lenguajes.children[11].style.opacity='.5';
-};
-if(scrollY > 1880 && scrollY < 2210){
-  lenguajes.children[12].style.opacity='1';
-  lenguajes.children[13].style.opacity='1';
-}else{
-  lenguajes.children[12].style.opacity='.5';
-  lenguajes.children[13].style.opacity='.5';
-};
-if(scrollY > 2180 && scrollY < 2495){
-  lenguajes.children[15].style.opacity='1';
-  lenguajes.children[16].style.opacity='1';
-}else{
-  lenguajes.children[15].style.opacity='.5';
-  lenguajes.children[16].style.opacity='.5';
-};
+//cada vez que hay un cambio en el hash ejecuto navegar, esto permite manejar el historial (yendo para adelante o atras en el navegador)
+window.addEventListener("hashchange", navegar);
 
 
+//----------------MATERIALIZE------------------------------
+//funcion para barra de navegacion lateral
+document.addEventListener('DOMContentLoaded', ()=>{
+   const elems = document.querySelectorAll('.sidenav');
+   const instances = M.Sidenav.init(elems);
+});
+
+//---------------------SCROLL--------------------------------
+
+const scrollTop = document.documentElement.scrollTop;
+window.addEventListener("scroll", ()=>{
+  const navbar = document.querySelector('.nav');
+  const logo = document.querySelector('.brand-logo');
+  const lenguajes = document.querySelector('.lenguajes');
+
+  //TRANSPARENCIA DE NAVBAR Y SALUDO
+    if(window.scrollY > 20){
+      navbar.classList.remove("transparent");
+      navbar.style.background = "rgba(245, 220, 220, .6)";
+      parrafo.style.opacity = '0';
+      h1.style.opacity = '0';
+    }else{
+      navbar.classList.add("transparent");
+      parrafo.style.opacity = '1';
+      h1.style.opacity = '1';
+    };
 })
-//------------DATOS PERSONALES-----------------
